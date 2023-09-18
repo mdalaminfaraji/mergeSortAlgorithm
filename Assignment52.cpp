911e03137a54eb950d43a5e7334d8e1b834da510
md. Alamin Faraji <alaminice1617@gmail.com>
2023-09-18 19:54:06 +0600

    assignment 5 done

--- Assignment52.cpp
+++ Assignment52.cpp
@@ -8,26 +8,23 @@ int main()
     {
         int n, m;
         cin >> n >> m;
-        int a[n];
+        int w[n];
         for (int i = 0; i < n; i++)
         {
-            cin >> a[i];
+            cin >> w[i];
         }
         int s = 1000 - m;
-
-        bool dp[n + 1][s + 1];
-        dp[0][0] = true;
+        int dp[n + 1][s + 1];
+        dp[0][0] = 0;
         for (int i = 1; i <= s; i++)
-        {
-            dp[0][i] = false;
-        }
+            dp[0][i] = INT_MAX - 1;
         for (int i = 1; i <= n; i++)
         {
             for (int j = 0; j <= s; j++)
             {
-                if (a[i - 1] <= j)
+                if (w[i - 1] <= j)
                 {
-                    dp[i][j] = dp[i][j - a[i - 1]] || dp[i - 1][j];
+                    dp[i][j] = min(1 + dp[i][j - w[i - 1]], dp[i - 1][j]);
                 }
                 else
                 {
@@ -35,11 +32,10 @@ int main()
                 }
             }
         }
-
-        if (dp[n][s])
-            cout << "YES" << endl;
+        if (dp[n][s] == INT_MAX - 1)
+            cout << -1 << endl;
         else
-            cout << "NO" << endl;
+            cout << dp[n][s] << endl;
     }
     return 0;
 }
--- Assignment53.cpp
+++ Assignment53.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+const long long int N = 1e9 + 7;
 int main()
 {
     int T;
@@ -8,38 +9,33 @@ int main()
     {
         int n, m;
         cin >> n >> m;
-        int a[n];
+        int w[n];
         for (int i = 0; i < n; i++)
         {
-            cin >> a[i];
+            cin >> w[i];
         }
         int s = 1000 - m;
-
-        bool dp[n + 1][s + 1];
-        dp[0][0] = true;
+        long long int dp[n + 1][s + 1];
+        dp[0][0] = 1;
         for (int i = 1; i <= s; i++)
-        {
-            dp[0][i] = false;
-        }
+            dp[0][i] = 0;
         for (int i = 1; i <= n; i++)
         {
             for (int j = 0; j <= s; j++)
             {
-                if (a[i - 1] <= j)
+                if (w[i - 1] <= j)
                 {
-                    dp[i][j] = dp[i][j - a[i - 1]] || dp[i - 1][j];
+                    dp[i][j] = dp[i][j - w[i - 1]] + dp[i - 1][j];
+                    dp[i][j] %= N;
                 }
                 else
                 {
                     dp[i][j] = dp[i - 1][j];
+                    dp[i][j] %= N;
                 }
             }
         }
-
-        if (dp[n][s])
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        cout << dp[n][s] << endl;
     }
     return 0;
 }
